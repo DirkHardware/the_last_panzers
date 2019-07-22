6c150c2329008bfc3641de211bfd5365d799a7c2
Flatiron School <email.the.anderson@gmail.com>
2019-07-22 15:00:38 -0400

    building routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/tanks/", to: "tanks#index", as: "tanks"
+  get "/tank/:id", to: "tanks#show", as: "tank"
 end
